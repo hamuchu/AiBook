de6d3ee696e93c7f617de68bf22e7d6d0e5f2c61
Tomohiro Ueno <b1400500@planet.kanazawa-it.ac.jp>
2019-01-15 01:08:02 +0900

    README.md 追加した

---
+++ FaceDetection/README.md
@@ -0,0 +1,24 @@
+## 顔と目の認識
+
+# インストール方法
+```
+pip install opencv-python
+```
+
+# 実行方法
+
+1. face_detection.pyがあるフォルダに移動する
+コマンド例
+```
+cd 移動したいフォルダ(今回はFaceDetectionフォルダ)
+```
+
+1. カメラをパソコンに接続する
+
+1. プログラムを実行する
+```
+python face_detection.py
+```
+
+
+
--- FaceDetection/face_detection.py
+++ FaceDetection/face_detection.py
@@ -1,10 +1,9 @@
 import cv2
-import numpy as np
 
 if __name__ == '__main__':
     
     ESC_KEY = 27    
-    INTERVAL= 33     # 待ち時間
+    INTERVAL = 33     # 待ち時間
     FRAME_RATE = 28  # fps
 
     DEVICE_ID = 0
@@ -12,26 +11,23 @@ if __name__ == '__main__':
     # カメラ映像取得
     cap = cv2.VideoCapture(DEVICE_ID)
 
-    # 保存ビデオファイルの準備
-    end_flag, c_frame = cap.read()
-    height, width, channels = c_frame.shape
-
     # Haar-like特徴分類器の読み込み
     face_cascade = cv2.CascadeClassifier('frontalface_default.xml')
     eye_cascade = cv2.CascadeClassifier('eye.xml')
     
-    num=0
+    end_flag, c_frame = cap.read()
+
     # 変換処理ループ
-    while end_flag == True:
-        num=num+1
+    while end_flag is True:
+        # 次のフレーム読み込み
+        end_flag, c_frame = cap.read()
         # イメージファイルの読み込み
         img=c_frame
         # グレースケール変換
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         # 顔を検知
         faces = face_cascade.detectMultiScale(gray)
-                
-        #print faces
+
         for (x,y,w,h) in faces:    
             # 検知した顔を矩形で囲む
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -42,8 +38,8 @@ if __name__ == '__main__':
             # 顔の中から目を検知
             eyes = eye_cascade.detectMultiScale(roi_gray)
             for (ex,ey,ew,eh) in eyes:
-                 #検知した目を矩形で囲む
-                 cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+                #検知した目を矩形で囲む
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
         # フレーム表示
         cv2.imshow("Raw Video", img)
@@ -53,8 +49,6 @@ if __name__ == '__main__':
         if key == ESC_KEY:
             break
 
-        # 次のフレーム読み込み
-        end_flag, c_frame = cap.read()
 
     # 終了処理
     cv2.destroyAllWindows()
